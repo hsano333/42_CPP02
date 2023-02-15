5ff4b3628890ccfeeb7afbf490f9feffc9d60b87
Hiroaki Sano <hsano@student.42tokyo.jp>
2023-02-16 04:01:50 +0900

    fixed ex01 main

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 #include <Fixed.hpp>
 
+using std::cout;
+using std::endl;
+
 int main(void)
 {
     Fixed a;
     Fixed b( a );
     Fixed c;
 
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+
+    cout << endl;
     a.setRawBits(1);
     c = a;
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
 
+    cout << endl;
     a.setRawBits(123);
     b.setRawBits(456);
     c.setRawBits(789);
