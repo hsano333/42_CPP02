a126256718bea7bb920f5a9e807326fde6aea6a9
Hiroaki Sano <hsano@student.42tokyo.jp>
2023-02-16 04:19:48 +0900

    fixed main

--- ex03/srcs/main.cpp
+++ ex03/srcs/main.cpp
@@ -6,15 +6,12 @@
 using std::cout;
 using std::endl;
 
-
-
-
 int main(void)
 {
     Point a = Point(-0.5f, 0);
     Point b = Point(55, 0);
     Point c = Point(0, 0.09f);
-    Point point = Point(0, 0.01f);
+    Point point = Point(0, 0.08f);
     if (bsp(a, b, c, point))
         cout << "point is in triangle" << endl;
     else
